57576fb2b0ff97ea1aec49bff1279616264ed1b7
Stanley Petaway <spetaway@yahoo.com>
2022-03-29 14:41:58 -0400

    Update s3-bucket.tf
    bucket added to code

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,11 +1,12 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+module "s3-bucket" {
+  source  = "app.terraform.io/mactechinvent/s3-bucket/aws"
+  version = "2.8.0"
 
-  bucket = "my-s3-bucket"
+  bucket = "${var.bucket_prefix}-my-s3-bucket"
   acl    = "private"
 
   versioning = {
     enabled = true
   }
-
+  
 }
